b30f894ecc63593f610aac3b6d06004a6a88c90b
Max Ooi Wei Xiang <wxo_rocket0123@hotmail.com>
2021-12-16 19:53:21 +0800

    C5W4

--- Course5/README.md
+++ Course5/README.md
@@ -5,6 +5,6 @@
 | 1 | [Data Extraction](https://github.com/wxo15/EPFL-functional-programming-in-scala/blob/main/Course5/observatory/1.-Data-extraction.md) | :heavy_check_mark: |
 | 2 | [Raw Data Visualization](https://github.com/wxo15/EPFL-functional-programming-in-scala/blob/main/Course5/observatory/2.-Raw-data-visualization.md) | :heavy_check_mark: |
 | 3 | [Interactive visualization](https://github.com/wxo15/EPFL-functional-programming-in-scala/blob/main/Course5/observatory/3.-Interactive-visualization.md) | :heavy_check_mark: |
-| 4 | [Data manipulation](https://github.com/wxo15/EPFL-functional-programming-in-scala/blob/main/Course5/observatory/4.-Data-manipulation.md) |  |
+| 4 | [Data manipulation](https://github.com/wxo15/EPFL-functional-programming-in-scala/blob/main/Course5/observatory/4.-Data-manipulation.md) | :heavy_check_mark: |
 | 5 | [Value-added information visualization](https://github.com/wxo15/EPFL-functional-programming-in-scala/blob/main/Course5/observatory/5.-Value-added-information-visualization.md) |  |
 | 6 | [Interactive user interface](https://github.com/wxo15/EPFL-functional-programming-in-scala/blob/main/Course5/observatory/6.-Interactive-user-interface.md) |  |
--- Course5/observatory/src/main/scala/observatory/Manipulation.scala
+++ Course5/observatory/src/main/scala/observatory/Manipulation.scala
@@ -4,14 +4,61 @@ package observatory
   * 4th milestone: value-added information
   */
 object Manipulation extends ManipulationInterface {
+  
+  class Grid {
+    
+    private var temps: Array[Temperature] = new Array[Temperature](360*180)
 
+    private def address(location: Location): Int = {
+      val x = location.lon + 180
+      val y = location.lat + 89
+      (y * 360 + x).toInt
+    }
+
+    def set(location: Location, temp: Temperature): Unit = {
+      temps(address(location)) = temp
+    }
+
+    def get(location: Location): Temperature = {
+      temps(address(location))
+    }
+
+    def precompute(temps: Iterable[(Location, Temperature)]): Unit = {
+      for {
+        lat <- Range(90, -90, -1)
+        lon <- -180 until 180
+      } set(Location(lat, lon), Visualization.predictTemperature(temps, Location(lat, lon)))
+    }
+
+    def merge(that: Grid): Grid = {
+      temps.indices.foreach(i => this.temps(i) += that.temps(i))
+      this
+    }
+
+    def /=(denominator: Double): Grid = {
+      temps = temps.map(_ / denominator)
+      this
+    }
+
+    def -=(that: GridLocation => Temperature): Grid = {
+      for {
+        lat <- Range(90, -90, -1)
+        lon <- -180 until 180
+      } set(Location(lat, lon), get(Location(lat, lon)) - that(GridLocation(lat, lon)))
+      this
+    }
+  }
+
+  
   /**
     * @param temperatures Known temperatures
     * @return A function that, given a latitude in [-89, 90] and a longitude in [-180, 179],
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
-    ???
+    val grid = new Grid()
+    grid.precompute(temperatures)
+    (gl: GridLocation) => grid.get(Location(gl.lat, gl.lon))
   }
 
   /**
@@ -20,7 +67,15 @@ object Manipulation extends ManipulationInterface {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
-    ???
+    val grid = temperaturess.par
+      .map({ case (i) => {
+        val grid = new Grid
+        grid.precompute(i)
+        grid
+      }})
+      .reduce((a: Grid, b: Grid) => a.merge(b))
+    grid /= temperaturess.size
+    (gl: GridLocation) => grid.get(Location(gl.lat, gl.lon))
   }
 
   /**
@@ -29,9 +84,10 @@ object Manipulation extends ManipulationInterface {
     * @return A grid containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
-    ???
+    val grid = new Grid()
+    grid.precompute(temperatures)
+    grid -= normals
+    (gl: GridLocation) => grid.get(Location(gl.lat, gl.lon))
   }
-
-
+  
 }
-
